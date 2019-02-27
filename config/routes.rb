a01bd25f9f456d9dbbc0eab606d9e3ed35971754
Dan <dkwagner@mtu.edu>
2019-02-26 22:09:36 -0600

    Skeleton homepage, selected color scheme, skeleton about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'home/index'
+  get 'contact_us', to: 'home#contact', as: :contact
+  get 'about_us', to: 'home#about_us', as: :about
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
